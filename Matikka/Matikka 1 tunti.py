b99cd09572b6f4f9cc5c456ef66a109859c50db4
eliaseide <eliasoeide@gmail.com>
2024-10-28 00:25:24 +0200

    Matikka 1 tunti

---
+++ Matikka/Matikka 1 tunti.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+print("Tehtävä 1 osa 1")
+
+a=2.493
+b=0.911
+
+print(np.degrees(a))
+print(np.degrees(b))
+
+
+print("Tehtävä 1 osa 2")
+
+c=137.7
+d=62.3
+
+print(np.radians(c))
+print(np.radians(d))
+
+print("Tehtävä 1 osa 3")
+
+degs=[30, 45, 60, 90, 120, 135, 150, 180, 270, 360 ]
+
+rads=[]
+
+for deg in degs:
+    rads.append(np.radians(deg))
+
+for rad in rads:
+    print(rad)
+
+print("Tehtävä 2 hypotenuusa juttu")
+
+f=1.6
+g=2.3
+
+h=np.hypot(f,g)
+
+print(h)
